6ba9aac8ab1c181e816498e5666b8b7fcde8d0f7
Nicolas Bologna <nicolasandresbologna@hotmail.com>
2022-02-28 17:11:45 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio1/Persona.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    class Persona
+    {
+        public Persona(string nombre)
+        {
+            Nombre = nombre;
+        }
+
+        public string Nombre { get; set; }
+
+        public override string ToString()
+        {
+            return $"Hola! mi nombre es {Nombre}.";
+        }
+    }
+}
---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/*Create a C# program that requests three names of people from the user and stores 
+ * them in an array of objects of type Person. To do this, first create a Person class 
+ * that has a Name property of type string and override the ToString() method.
+
+End the program by reading people and executing the ToString() method on the screen.*/
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona[] personas = new Persona[3];
+            for (int i = 0; i < 3; i++)
+            {
+                personas[i] = new Persona(Console.ReadLine());
+            }
+
+            foreach (var persona in personas)
+            {
+                Console.WriteLine(persona.ToString());
+            }
+        }
+    }
+}
